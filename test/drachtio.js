7ace7f3e4abf72d25e14870aaef46fdb5024c5c0
Dave Horton <daveh@beachdognet.com>
2015-03-21 21:48:50 -0400

    update to latest version of drachtio-test-fixtures

--- test/drachtio.js
+++ test/drachtio.js
@@ -2,32 +2,11 @@ var assert = require('assert');
 var spawn = require('child_process').spawn;
 var exec = require('child_process').exec;
 var should = require('should');
-var merge = require('merge') ;
-var fs = require('fs') ;
 var debug = require('debug')('drachtio-client') ;
-var async = require('async') ;
 var Agent = require('drachtio-client').Agent ;
 var fixture = require('drachtio-test-fixtures') ;
 var uac, uas ;
 var cfg = fixture(__dirname,[9022,9023,9024],[6060,6061,6062]) ;
-
-function configureUac( config ) {
-    uac = new Agent(function(req,res){}) ;
-    uac.set('api logger',fs.createWriteStream(config.apiLog) ) ;
-    uac.connect(config.connect_opts) ;
-    return uac ;
-}
-function connectAll( agents, cb ) {
-    async.each( agents, function( agent, callback ) {
-        if( agent.connected ) agent.disconnect() ;
-        agent.on('connect', function(err) {
-            return callback(err) ;
-        }) ;
-    }, function(err) {
-        if( err ) return cb(err) ;
-        cb() ;
-    }) ;
-}
 
 describe('drachtio', function() {
     this.timeout(6000) ;
@@ -40,9 +19,9 @@ describe('drachtio', function() {
     }) ;
 
     it('must be able to reject an INVITE', function(done) {
-        uac = configureUac( cfg.client[0] ) ;
+        uac = cfg.configureUac( cfg.client[0], Agent ) ;
         uas = require('../examples/logger/app')(cfg.client[1]) ;
-        connectAll([uac, uas], function(err){
+        cfg.connectAll([uac, uas], function(err){
             uac.request({
                 uri: cfg.sipServer[1],
                 method: 'INVITE',
@@ -59,9 +38,9 @@ describe('drachtio', function() {
         }) ;
     }) ;
     it('must set response time in a custom header', function(done) {
-        uac = configureUac( cfg.client[0] ) ;
+        uac = cfg.configureUac( cfg.client[0], Agent ) ;
         uas = require('../examples/invite-non-success/app')(cfg.client[1]) ;
-        connectAll([uac, uas], function(err){
+        cfg.connectAll([uac, uas], function(err){
             uac.request({
                 uri: cfg.sipServer[1],
                 method: 'INVITE',
